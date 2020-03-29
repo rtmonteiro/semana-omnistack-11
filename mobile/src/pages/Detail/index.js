2aaea4613a47d5e82995a67936bb5e7ca688e324
rtmonteiro <ryan.monteiro@aluno.ufes.br>
2020-03-28 23:41:19 -0300

    Fixed the message position to receive incident values

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -11,11 +11,10 @@ import styles from './styles';
 export default function Detail() {
   const navigation = useNavigation();
   const route = useRoute();
-  const message = `Olá ${incident.name}, estou entrando em contato pois gostaria de ajudar no caso: '${incident.title}' com o valor de ${Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'}).format(incident.value)}`;
-  const phoneNumber = '5527981149278';
-
-
+  
   const incident = route.params.incident;
+
+  const message = `Olá ${incident.name}, estou entrando em contato pois gostaria de ajudar no caso: '${incident.title}' com o valor de ${Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'}).format(incident.value)}`;
 
   function navigateBack() {
     navigation.goBack();
